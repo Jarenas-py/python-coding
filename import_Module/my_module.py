0c81b934b0041185f4a5362285cd208fa6ccd8db
Whon Jick <professionaljarenas@gmail.com>
2026-01-10 22:33:21 +0800

    working on importing modules source code. WIll update try_Except.py after finishing this.

--- import_Module/my_module.py
+++ import_Module/my_module.py
@@ -3,35 +3,13 @@ print("This is the print statement from my_module.py!")
 def name_call(name):
     return f"Hello world! My name is {name}"
 
-def ohms_law(num1, num2, choice, choice2):
-    
-    while True:
-        print("Ohm's Law!\n")
-
-        while True: 
-            try:
-                if choice == "VOLTAGE" or "AMPERES" or "RESISTANCE":
-                    break
-            except word_error:
-                print("This is not a valid input")
-                continue
-
+def ohms_law(choice, num1, num2):
         if choice == "VOLTAGE":
             v = num1 * num2
             print(f"The voltage is {v}V")
-        elif choice == "AMPERES":
+        elif choice == "CURRENT":
             a = num1 / num2
             print(f"The current in amperes is {a}A")
         else:
             r = num1 / num2
             print(f"The resistance in ohms is {r}ohms")
-
-        while True:
-            try:
-                if choice2 == "N":
-                    print("Thank you for using our program!")
-                    break
-            except word_error:
-                print("This is not a valid input!")
-                continue
-
--- import_Module/python_Module.py
+++ import_Module/python_Module.py
@@ -11,3 +11,58 @@ y = "This is the print statement from this file part 2!"
 print(y)
 
 import my_module2 as mm2
+
+#Given a module that has multiple user defined functions and you want to import only that specific function for use in your separate python code base, you can utilize the "from and import" in order to do just that. A developer can also stack the "as" syntax in order to give that specific user-defined function from another module a nickname for easier access.
+
+from my_module import ohms_law as ol
+
+while True:
+    print("\nOhm's Law")
+    
+    while True:
+
+        try:
+            print("What variable are you trying to find? (VOLTAGE, CURRENT or RESISTANCE)")
+            ohms_choice = input().upper()
+            if ohms_choice == "VOLTAGE" or "CURRENT" or "RESISTANCE":
+                break
+        except input_error:
+            print("Invalid input. Only enter VOLTAGE, CURRENT, or  RESISTANCE")
+
+    while True:
+        if ohms_choice == "VOLTAGE":
+            try:
+                val1 = float(input("Enter the resistance in Ohms: "))
+                val2 = float(input("Enter the current in Amperes: "))
+                break
+            except input_error:
+                print("Only enter numbers.")
+        elif ohms_choice == "CURRENT":
+            try:
+                val1 = float(input("Enter the voltage in Volts: "))
+                val2 = float(input("Enter the resistance in Ohms: "))
+            except input_error:
+                print("Enter a valid number.")
+            try:
+                val1 = float(input("Enter the voltage in Volts: "))
+                val2 = float(input("Enter the current in Amperes: "))
+                break
+            except input_error:
+                print("Enter a valid number.")
+
+    ol(ohms_choice, val1, val2)
+
+    while True:
+        try:
+            again_choice = input("Do you want to use again? (Y/N): ").upper()
+            if again_choice == "Y" or "N":
+                break
+        except input_error:
+            print("Enter a valid input. Only enter Y or N")
+
+    if again_choice == "N":
+        print("Thank you for using the program!")
+        break
+    else:
+        continue
+
