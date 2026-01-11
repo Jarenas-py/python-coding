f373ca343b23cb05a04799ce80fa635e5e633b8f
Whon Jick <professionaljarenas@gmail.com>
2026-01-11 21:10:44 +0800

    Added pip notes

---
+++ python_pip/pip.py
@@ -0,0 +1,33 @@
+#pip stands for "PIP installs Packages". It is the official package manager of Python which is responsible for downloading and installing third party and open source libraries for Python.
+#Important Commands:
+#pip install (name of library) : Downloads and installs the library in question.
+#pip list : shows the current libraries installed through pip
+#pip uninstall (name of library) : self-explanatory
+#pip list -o : shows the version of your pip and the latest global version of pip
+#pip install -U setuptools : updates your pip manager to the latest global version.
+#pip freeze : shows the version of all of your installed libraries.
+#pip -U install (name of library) : Updates a library to the latest version
+#pip search (name of library) : Shows if there exists a library with the name a user has entered.
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
