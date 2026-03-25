3f76699dbabecb9e639a48a9cc0ce1060fef2594
Jarenas-py <professionaljarenas@gmail.com>
2026-03-26 01:10:59 +0800

    updated basics

---
+++ Python Practice/basic_Functions.py
@@ -0,0 +1,8 @@
+message = "hello, my name is GITHUB. HELLO hello hello" 
+print(message.upper())
+print(message.lower())
+print(message.count("hello"))
+print(message.find("GITHUB"))
+
+message = message.replace("hello, my name is GITHUB. HELLO hello hello", "My name is Joseph!")
+print(message)
--- string_Manipulation/basic_Functions.py
+++ string_Manipulation/basic_Functions.py
@@ -1,8 +1,8 @@
 message = "He's got the whole world, in His hands."
-print(message.upper())
-print(message.lower())
-print(message.count("Hello"))
-print(message.find("world"))
+print(message.upper()) #Changes the message value to all capital letters.
+print(message.lower()) #Changes the message value to all lower case letters.
+print(message.count("Hello")) #Finds the string "Hello". If none, it prints 0. If there is, it prints the interger of how many that string is is in the value of the variable message.
+print(message.find("world")) #Finds the string world and finds the its index in the value of the variable message.
 
-message = message.replace("He's got the whole world, in His hands.", "Hello world!")
+message = message.replace("He's got the whole world, in His hands.", "Hello world!") #Replaces the whole value of message. It takes 2 positional arguments. The first argument is the initial argument while the second argumnt is the argument for replacing the initial value.
 print(message)
