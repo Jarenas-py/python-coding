56a20b5f3c758248c2b2f255ec3c42fbdc792f72
Jarenas-py <professionaljarenas@gmail.com>
2026-03-29 21:24:42 +0800

    practice for data_Type.py

---
+++ Python Practice/data_Type.py
@@ -0,0 +1,16 @@
+var1 = 12
+var2 = 3.14
+var3 = "Hello, World!"
+
+string1 = "DATA TYPES IN PYTHON\n"
+string2 = f"""Values:
+var1 = {var1}
+var2 = {var2}
+var3 = {var3}\n"""
+string3 = f"""1. var1 is a {type(var1)}
+2. var2 is a {type(var2)}
+3. var3 is a {type(var3)}"""
+
+print(string1)
+print(string2)
+print(string3)
