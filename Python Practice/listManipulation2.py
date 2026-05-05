cf9dbf8cd94cd4c66b73be9fbc4263507e6ef308
Jarenas-py <professionaljarenas@gmail.com>
2026-05-05 14:46:47 +0800

    asfsafsadf

--- Python Practice/listManipulation2.py
+++ Python Practice/listManipulation2.py
@@ -34,15 +34,41 @@ print("=======================\n")
 #Your Starting Variables:
 #Copy and paste these exact variables at the top of your code to begin:
 
-raw_inventory_string = "Webcam,Monitor,Keyboard,Mouse,Headset"
-prices = [45, 120, 35, 20, 85, 10, 250]
+raw_inventory_string = "Webcams,Monitors,Keyboards,Mouses,Headsets"
+prices = [45, 120, 35, 85, 250]
 
 inventory = raw_inventory_string.split(",")
 inventory.sort()
-prices.reverse()
-
-
-
+prices.sort(reverse = "True")
 
 print(inventory)
 print(prices)
+loop_prices = 0
+
+print(f"""
+The cheapest price in the store is {min(prices)}
+The most expensive price in the store is {max(prices)}
+The total sum of all the prices combined is {sum(prices)}""")
+
+print(f"""\nWELCOME TO CDR-KING
+We have: {" *** ".join(inventory)}
+
+Here are the prices of each product:""")
+    
+for i, j in enumerate(inventory, start = 1):
+    print(i, j)
+    print(f"₱{prices[loop_prices]}\n")
+    loop_prices += 1
+
+    if loop_prices == len(inventory):
+        break
+    else:
+        continue
+
+print(f"""\nThe cheapest price in the store is: {inventory[prices.index(min(prices))]}= ₱{min(prices)}
+The most expensive price in the store is: {inventory[prices.index(max(prices))]}= ₱{max(prices)}
+The total sum of all the prices combined is: ₱{sum(prices)}""")
+
+search = input("Search: ").lower()
+
+if 
