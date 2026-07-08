53e938d3f637db567b5e6ee0306ebd1a01999908
Jarenas-py <professionaljarenas@gmail.com>
2026-07-08 14:38:20 +0800

    last commit for the day

--- loops_Iterations/while_Loop.py
+++ loops_Iterations/while_Loop.py
@@ -1,4 +1,9 @@
-#A while loop gives a developer the tools to loop a lines of code and can stop it given the condition that comes with the while loop. One must take note however that while loops do not have the self-iterating functionalities that for loop has. Because of this, a variable that would serve as a tracker and an incrementer inside a while loop can be utilized in order to meet the conditions of the while loop to break the loop in that manner.
+#A while loop gives a developer the tools to loop a lines of code and can stop
+#  it given the condition that comes with the while loop. One must take note 
+# however that while loops do not have the self-iterating functionalities that 
+# for loop has. Because of this, a variable that would serve as a tracker and 
+# an incrementer inside a while loop can be utilized in order to meet the conditions 
+# of the while loop to break the loop in that manner.
 
 x = 0
 
@@ -6,7 +11,8 @@ while x < 11:
     print(x)
     x += 1
 
-#The while True condition runs the while loop indefinitely unless a developer has specified a condition inside the while loop statement to break out of the loop
+#The while True condition runs the while loop indefinitely unless a developer 
+# has specified a condition inside the while loop statement to break out of the loop
 
 i = 0
 
@@ -16,4 +22,15 @@ while True:
 
     if i == 10000:
         print(f"The variable i has reached the value {i}. Ending the while loop!")
+        break
+
+print("\n ============NEW TRAINING=============")
+numeros = range(1, 21)
+x = 0
+while x < 5:
+    print(numeros[x])
+    x += 1
+
+    if x == 4:
+        print(f"The last number is {numeros[x]}")
         break
