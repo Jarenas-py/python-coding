84f169370237a18265e81636f846e9d7278cf040
Jarenas-py <professionaljarenas@gmail.com>
2026-07-17 22:29:22 +0800

    Python Module Index

---
+++ Python Modules/Python Module Index.py
@@ -0,0 +1,3 @@
+#Python has more modules from its standard library as well as more niche modules that are made by the Python community.
+#The following link shows a list of standard modules from the standard python library.
+# PYTHON DOCUMENTATION - PYTHON STANDARD LIBRARY MODULES: https://docs.python.org/3/py-modindex.html
