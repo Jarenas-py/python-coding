8d2733e0efcc349538efe7e0bdda48ab1c2d9741
Jarenas-py <professionaljarenas@gmail.com>
2026-08-12 10:07:59 +0800

    unfinished inheritance notes

--- python_oop/classesInheritance.py
+++ python_oop/classesInheritance.py
@@ -20,3 +20,26 @@ car2 = Mitsubishi("Supra", "White", "2001", "1000000")
 
 print(car1.fullReturn())
 print(car2.fullReturn())
+
+# When trying to create a subclass and making its
+# own __init__ method by inheriting the parent
+# class' __init__ method and adding another
+# argument on the subclass __init__ method, the
+# following code would be the exact methodology 
+# to achieve that. "super().__init__(model, color, year, value)"
+# simply tells the parent class that the following
+# arguments stated inside the parenthesis are to
+# be initialized by the __init__ method of the
+# parent class.
+
+class Subaru(Ford):
+    def __init__(self, model, color, year, value, milleage):
+        super().__init__(model, color, year, value)
+        self.milleage = milleage
+
+    def subaruReturn(self):
+        return f"{self.model} {self.color} {self.year} {self.value} {self.milleage}"
+
+car3 = Subaru("Impreza", "Blue", "1999", "800000", "500000")
+print(car3.subaruReturn())
+
