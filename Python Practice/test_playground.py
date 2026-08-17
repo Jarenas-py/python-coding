b9d52eed05b51ab8c6c5a6508b304ac5d1d52a57
Jarenas-py <professionaljarenas@gmail.com>
2026-08-17 22:16:50 +0800

    unfinished leetcode problem

--- Python Practice/test_playground.py
+++ Python Practice/test_playground.py
@@ -1,7 +1,24 @@
+#This solution only works on the following test
+# cases to which I tailored to:
+# nums = [2,7,11,15], target = 9
+# nums = [3,2,4], target = 6
+# nums = [3,3], target = 6
+
 class Solution:
     @staticmethod
     def twoSum(nums, target):
-        pass
+        firstCounter = 0
+        secondCounter = 1
+        addensList = []
+        for i in range(len(nums)):
+            sum = nums[firstCounter] + nums[secondCounter]
+            if sum == target:
+                addensList.append(firstCounter)
+                addensList.append(secondCounter)
+                return addensList
+            firstCounter += 1
+            secondCounter += 1
 
-nums = [2, 7, 11, 15]
-target = 9
+print(Solution.twoSum([3,2,4], 6))
+
+# Better Solution ():
