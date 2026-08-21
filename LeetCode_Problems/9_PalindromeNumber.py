ff7e0d76068fa6edef6e041812ac74058bb2c34b
Jarenas-py <professionaljarenas@gmail.com>
2026-08-21 21:22:47 +0800

    Optimized Plalindrome Number algorithm

--- LeetCode_Problems/9_PalindromeNumber.py
+++ LeetCode_Problems/9_PalindromeNumber.py
@@ -12,5 +12,52 @@ class Solution:
         reverse = "".join(reverseList)
         return reverse == x
 
+print(Solution.isPalindrome(121))
 
-print(Solution.isPalindrome(1234512))
+# Optimized Version (Based by NeetCode)
+
+# The following solution focuses on utilizing math
+# instead of converting the input "x" into a string
+# in order to determine if input "x" is a Palindrome
+# number.
+
+# In this instance, assume that input "x" = 121.
+
+class Solution2:
+     @staticmethod
+     def twoSum2(x):
+
+        # Step 1: Check if x is negative. If it is, it is not
+        # a Palindrome Number so immediately return false.
+        if x < 0: return False
+
+        # Step 2: Set up the divider that would serve as 
+        # reference later on on the codebase for both checking
+        # the topmost value of input x and when removing it 
+        # as well for the next number. In the case of x = 121;
+
+        # First Iteration:
+        # 121 > 10 * 1. Therefore, 1 * 10 = div = 10
+
+        # Second Iteration:
+        # 121 > 10 * 10. Therefore, 10 * 10 = div = 100
+
+        # Third Iteration:
+        # 121 < 10 * 100. Therefore, while loop ends. div = 100.
+        div = 1
+        while x >= 10 * div:
+            div *= 10
+
+        # Step 3: Check if current first and last numbers are
+        # not equal. If not, return False.
+        while x:
+            if x // div != x % 10: return False
+
+        # Step 4: Strip away the first and last numbers in 
+        # preparation for the next while iteration.
+
+            x = (x % div) // 10
+            div = div / 100
+        return True
+
+print(Solution2.twoSum2(121))
