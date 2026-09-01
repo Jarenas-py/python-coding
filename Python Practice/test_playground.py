b3a8eb8bbd08c0f38675bd8306c1f6262f3e3f71
Jarenas-py <professionaljarenas@gmail.com>
2026-09-01 21:03:52 +0800

    unfinished problem 258

--- Python Practice/test_playground.py
+++ Python Practice/test_playground.py
@@ -1,12 +1,4 @@
-num = 38
-output = 0
-numString = str(num)
-while len(str(output)) == 1:
-    for i in numString:
-        i = int(i)
-        output += i
-
-    numString = str(output)
-    print(output)
-    continue
-print(output)
+class Solution:
+    def addDigits(num):
+        while True:
+            pass
---
+++ Python Practice/test_playground2.py
@@ -0,0 +1,2 @@
+num = 38
+print(num//10)
